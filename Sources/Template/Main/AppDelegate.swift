1e37d3baad429e54b51ce40481262d423f07a759
Raul Max <raulmax319@gmail.com>
2022-08-10 08:31:10 -0300

    feat: ios app source files

---
+++ Sources/Template/Main/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+// AppDelegate.swift
+// Template
+//
+// Created by Raul Max on 07/23/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = UINavigationController(rootViewController: ViewController())
+    window.makeKeyAndVisible()
+    
+    self.window = window
+
+    return true
+  }
+}
---
+++ Sources/Template/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Raul Max on 23/07/22.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemRed
+  }
+}
