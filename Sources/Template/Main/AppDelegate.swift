b00fdcd7c6230badad2cd4fb19e7c48409eb0fbe
Raul Max <raulmax319@gmail.com>
2022-11-13 20:06:49 -0300

    chore: lint

--- Sources/Template/Main/AppDelegate.swift
+++ Sources/Template/Main/AppDelegate.swift
@@ -11,12 +11,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
     let window = UIWindow(frame: UIScreen.main.bounds)
-    window.rootViewController = UINavigationController(rootViewController: ViewController())
+    window.rootViewController = UINavigationController(
+      rootViewController: ViewController())
     window.makeKeyAndVisible()
-    
+
     self.window = window
 
     return true
